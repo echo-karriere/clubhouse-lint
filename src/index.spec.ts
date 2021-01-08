bb730c99deda73311af6135fde3e6b75e02b917e
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-01-08 16:05:21 +0100

    Create check for commit message format

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,38 @@
-import { hello } from ".";
+import { checkCommit } from ".";
 
-test("hello", () => {
-  expect(hello()).toEqual("Hello world!");
-  expect(hello("Ola")).toEqual("Hello Ola!");
+describe("checkCommit", () => {
+  describe("allows correct commits", () => {
+    it("properly formatted", () => {
+      expect(checkCommit("[CH-123]: test message")).toBeUndefined();
+    });
+
+    it("without Clubhouse references", () => {
+      expect(checkCommit("Fix commit message")).toBeUndefined();
+    });
+  });
+
+  describe("disallows incorrect commits", () => {
+    it("lowercase GH-", () => {
+      expect(() => checkCommit("[ch-123]: wrong")).toThrow(/The reference should be upper case/);
+    });
+
+    it("mixed case GH-", () => {
+      expect(() => checkCommit("[Ch-123]: wrong")).toThrow(/The reference should be upper case/);
+      expect(() => checkCommit("[cH-123]: wrong")).toThrow(/The reference should be upper case/);
+    });
+
+    it("missing dash", () => {
+      expect(() => checkCommit("[CH123]: wrong")).toThrow(/The reference should have a dash/);
+    });
+
+    it("fails commit from git helper", () => {
+      expect(() => checkCommit("Create husky hook for commit message linting [ch97]")).toThrow(
+        /You should start your commit message/,
+      );
+    });
+
+    it("missing colon", () => {
+      expect(() => checkCommit("[CH-123] wrong")).toThrow(/Your reference should have a colon/);
+    });
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,38 @@
-export const hello = (name = "world"): string => {
-  return `Hello ${name}!`;
+import fs from "fs";
+
+export const main = (): void => {
+  if (!process.env.HUSKY_GIT_PARAMS) throw new Error("Can't find $HUSKY_GIT_PARAMS");
+  const commit = readFile(process.env.HUSKY_GIT_PARAMS);
+  return checkCommit(commit);
 };
+
+const RE_CORRECT_REF = RegExp(/^\[CH-\d+\]:/);
+const RE_WRONG_CASE_REF = RegExp(/^\[(ch|cH|Ch)(-|)\d+\]:/);
+const RE_MISSING_DASH_REF = RegExp(/^\[CH\d+\]:/);
+const RE_CH_COMMIT_HELPER_REF = RegExp(/\[ch\d+\]/);
+const RE_MISSING_COLON = RegExp(/^\[CH-\d+\]\s/);
+
+export const checkCommit = (commit: string): void => {
+  if (!commit.toLowerCase().includes("[ch")) return;
+  if (RE_CORRECT_REF.exec(commit) !== null) return;
+
+  if (RE_WRONG_CASE_REF.exec(commit) !== null) {
+    throw new Error(`The reference should be upper case: '[CH-123]', not '[ch-123]'`);
+  }
+
+  if (RE_MISSING_DASH_REF.exec(commit) !== null) {
+    throw new Error(`The reference should have a dash: '[CH-123]', not '[CH123]'`);
+  }
+
+  if (RE_CH_COMMIT_HELPER_REF.exec(commit) !== null) {
+    throw new Error(`You should start your commit message like this: '[CH-123]: <message>'`);
+  }
+
+  if (RE_MISSING_COLON.exec(commit) !== null) {
+    throw new Error(`Your reference should have a colon at the end: '[CH-123]:', not '[CH-123]'`);
+  }
+
+  throw new Error(`Unrecognized commit, aborting... Use '--no-verify' if this is a mistake`);
+};
+
+const readFile = (filename: string): string => fs.readFileSync(filename, { encoding: "utf-8" });
