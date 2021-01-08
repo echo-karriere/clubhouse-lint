5ce1be3d71cd3b509f045fc610ec02174e84ef78
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-01-08 16:33:42 +0100

    [CH-97]: add comments

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import fs from "fs";
 
+/**
+ * Main entry point of linter, checks that the environment variable exists and
+ * then runs the check if it does. Does not work outside of `husky`.
+ */
 export const main = (): void => {
   if (!process.env.HUSKY_GIT_PARAMS) throw new Error("Can't find $HUSKY_GIT_PARAMS");
   const commit = readFile(process.env.HUSKY_GIT_PARAMS);
@@ -12,13 +16,19 @@ export const main = (): void => {
   }
 };
 
+// Regex for a correct reference `[CH-123]:`
 const RE_CORRECT_REF = RegExp(/^\[CH-\d+\]:/);
+// Regex for when the reference has the wrong case: `[ch-123]`
 const RE_WRONG_CASE_REF = RegExp(/^\[(ch|cH|Ch)(-|)\d+\]:/);
+// Regex for when the reference is missing a dash: `[CH123]:`
 const RE_MISSING_DASH_REF = RegExp(/^\[CH\d+\]:/);
+// Regex for when you've copied the commit from Clubhouse: `Commit message [ch123]`
 const RE_CH_COMMIT_HELPER_REF = RegExp(/\[ch\d+\]/);
+// Regex for when the message is missing a colon: `[CH-123]`
 const RE_MISSING_COLON = RegExp(/^\[CH-\d+\]\s/);
 
 export const checkCommit = (commit: string): void => {
+  // If the commit is correct or does not contain a reference, it is valid.
   if (!commit.toLowerCase().includes("[ch")) return;
   if (RE_CORRECT_REF.exec(commit) !== null) return;
 
