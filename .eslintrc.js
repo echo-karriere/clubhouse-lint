f1a1a54cc1f7de3e50f1667a1f0088ba4d0c1df7
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-02-04 17:17:55 +0100

    Fix ESLint warnings, disable no-process-exit rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,4 +4,8 @@ module.exports = {
     project: "./tsconfig.json",
   },
   extends: ["@sondr3/typescript", "plugin:node/recommended-module", "plugin:jest/recommended", "plugin:jest/style"],
+  rules: {
+    "no-process-exit": "off",
+    "unicorn/no-process-exit": "off",
+  },
 };
